db37f3950bfd9ed96710f01cfac2741502d2c1e7
208710961 <armoreraca@est.utn.ac.cr>
2026-03-20 18:32:26 -0600

    Cabmbio 15: Mejoras en la usabilidad, maquitacion y pagina de mensajes

--- JS/layout.js
+++ JS/layout.js
@@ -28,3 +28,76 @@ fetch(`${basePath}/HTML/Layout/footer.html`)
     if (footer) footer.innerHTML = data;
   })
   .catch(error => console.error("Error cargando footer:", error));
+/* =========================
+   SECCIONES
+========================= */
+
+fetch(`${basePath}/HTML/ConfiguracionBasica/volumen.html`)
+  .then(res => res.text())
+  .then(data => {
+    const contenedor = document.getElementById("seccion-volumen");
+    if (contenedor) {
+      contenedor.innerHTML = data;
+    }
+  });
+fetch(`${basePath}/HTML/ConfiguracionBasica/brillo.html`)
+  .then(res => res.text())
+  .then(data => {
+    const contenedor = document.getElementById("seccion-brillo");
+    if (contenedor) {
+      contenedor.innerHTML = data;
+    }
+  });
+fetch(`${basePath}/HTML/ConfiguracionBasica/letra.html`)
+  .then(res => res.text())
+  .then(data => {
+    const contenedor = document.getElementById("seccion-letra");
+    if (contenedor) {
+      contenedor.innerHTML = data;
+    }
+  });
+fetch(`${basePath}/HTML/ConfiguracionBasica/teclado.html`)
+  .then(res => res.text())
+  .then(data => {
+    const contenedor = document.getElementById("seccion-teclado");
+    if (contenedor) {
+      contenedor.innerHTML = data;
+    }
+  });
+
+
+
+fetch(`${basePath}/HTML/WhatsApp/mensajes.html`)
+  .then(res => res.text())
+  .then(data => {
+    const contenedor = document.getElementById("seccion-mensajes");
+    if (contenedor) {
+      contenedor.innerHTML = data;
+    }
+  });
+
+function mostrarSeccion(id) {
+  // NO ocultar el menú ❌ (quitar esta línea)
+  // document.getElementById("menu").style.display = "none";
+
+  // Oculta todas las secciones
+  const secciones = ["volumen", "brillo", "letra", "teclado"];
+  secciones.forEach(sec => {
+    document.getElementById(sec).classList.add("hidden");
+  });
+
+  // Muestra la seleccionada
+  document.getElementById(id).classList.remove("hidden");
+
+  window.scrollTo({ top: 300, behavior: "smooth" }); // baja un poco al contenido
+}
+function volverMenu() {
+  document.getElementById("menu").style.display = "grid";
+
+  const secciones = ["volumen", "brillo", "letra", "teclado"];
+  secciones.forEach(sec => {
+    document.getElementById(sec).classList.add("hidden");
+  });
+
+  window.scrollTo({ top: 0, behavior: "smooth" });
+}
